f983248444dd96f2ba5a6f21003825601e921a8e
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2014-12-22 14:44:16 +0100

    Add tests for debug wrapper

--- test/test.sh
+++ test/test.sh
@@ -7,6 +7,7 @@ DEBUG_COLORS=no $srcdir/../bin/test &> $srcdir/out0.tmp
 DEBUG_COLORS=no DEBUG=main $srcdir/../bin/test &> $srcdir/out1.tmp 
 DEBUG=main $srcdir/../bin/test &> $srcdir/out1c.tmp 
 DEBUG_COLORS=no DEBUG=* $srcdir/../bin/test &> $srcdir/out2.tmp 
+DEBUG_COLORS=no DEBUG=* $srcdir/../bin/test2 &> $srcdir/out3.tmp 
 
 function check {
 	m=$1
@@ -23,6 +24,7 @@ function check {
 check '1' $srcdir/out0.tmp $srcdir/out0-ref.tmp 
 check '2' $srcdir/out1.tmp $srcdir/out1-ref.tmp 
 check '3' $srcdir/out2.tmp $srcdir/out2-ref.tmp 
+check '4' $srcdir/out3.tmp $srcdir/out3-ref.tmp
 check '5' $srcdir/out1c.tmp $srcdir/out1c-ref.tmp
 
 
---
+++ test/test3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "debug.hxx"
+
+using namespace std;
+
+extern void secondfunction();
+
+int main(int argc, char const *argv[])
+{
+	debugm("this is main");
+	return 0;
+}
